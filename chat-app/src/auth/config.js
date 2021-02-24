8f7f3cb3c075f05bc429eb2ef16a7cff36c9efe7
skvisgaard <skvisgaard@gmail.com>
2021-02-24 13:50:32 +0100

    Deleted old project version

--- chat-app/src/auth/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    clientId: "XvVd04tlODCnxvM1AR37QNgWw4cw9QBp",
-    domain: "dev-skvisgaard.auth0.com",
-    redirect: "http://localhost:3000/close-popup",
-    logoutUrl: "http://localhost:3000",
-    audience: "https://chat-app-api"
-  };
