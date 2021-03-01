08019e0de716042f0d75754ed3a1fe0b695e3c39
skvisgaard <skvisgaard@gmail.com>
2021-03-01 11:02:31 +0100

    add firebase credentials

--- talk_to_me/src/firebase.js
+++ talk_to_me/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBARE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyCGPozXn-gK8q0Suv6IY35VLjE86u1twx8",
+    authDomain: "talktome-ec7f2.firebaseapp.com",
+    databaseURL: "https://talktome-ec7f2-default-rtdb.firebaseio.com",
+    projectId: "talktome-ec7f2",
+    storageBucket: "talktome-ec7f2.appspot.com",
+    messagingSenderId: "693873571305",
+    appId: "1:693873571305:web:b9cfeae4cbb02bb4a8c778"
 });
 
 export const auth = app.auth();
